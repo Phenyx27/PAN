b3aa239adf7f1f9052070340d6d1eabf7f09e25a
Phenyx27 <antoinesauve27@gmail.com>
2026-02-07 18:10:23 +0100

    Add files via upload

---
+++ pan/header.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="header.css">
+
+<h1>Portail des Antiquités Numériques</h1>
---
+++ pan/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<link rel=stylesheet href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="index.css">
+
+	<title>Ceci est une page HTML de test</title>
+</head>
+<header>
+	<?php include 'header.php'; ?>
+</header>
+
+<body>
+	<h2>Page de test</h2>
+	<background style="color ; yellow;"></style> 
+
+	<p>
+		Cette page contient <strong>uniquement</strong> du code HTML.<br />
+
+
+		Voici quelques petits tests :
+
+
+	</p>
+	<?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
+	<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+	<ul>
+		<li style="color: blue;">Texte en bleu</li>
+		<li style="color: red;">Texte en rouge</li>
+		<li style="color: green;">Texte en vert</li>
+	</ul>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col" id="col1">col</div>
+			<div class="col" id="col2">col
+
+			</div>
+			<div class="col" id="col3">col</div>
+			<div class="col" id="col4">col</div>
+		</div>
+		<div class="row">
+			<div class="col-8" id="col5">
+				<!-- carrousel -->
+				<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+					<div class="carousel-indicators">
+						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+							class="active" aria-current="true" aria-label="Slide 1"></button>
+						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+							aria-label="Slide 2"></button>
+						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+							aria-label="Slide 3"></button>
+					</div>
+
+					<div class="carousel-inner">
+						<div class="carousel-item active" data-bs-interval="4000">
+							<img src="image chat.webp" class="d-block w-100" alt="Slide 1">
+						</div>
+
+						<div class="carousel-item" data-bs-interval="5000">
+							<img src="rome.jpg" class="d-block w-100" alt="Slide 2">
+						</div>
+
+						<div class="carousel-item" data-bs-interval="4000">
+							<img src="senat.jpg" class="d-block w-100" alt="Slide 3">
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-4">
+				col-4
+
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- pour les animations -->
+	<!--jquery-->
+	<script src="https://code.jquery.com/jquery-3.7.1.js"
+		integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+	<!--bootstrap js -->
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
